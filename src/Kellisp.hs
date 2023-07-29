483cfc92c0303b3494d3d444f7dff73378d517cb
Zack Eisbach <zack.eisbach@gmail.com>
2023-07-29 13:26:03 -0400

    add cmd line history with haskeline

--- src/Kellisp.hs
+++ src/Kellisp.hs
@@ -1,31 +1,40 @@
-{-|
-Contains all outward-facing functions used to interact with Kellisp.
-For now, this is limited to REPL functions.
--}
+-- |
+-- Contains all outward-facing functions used to interact with Kellisp.
+-- For now, this is limited to REPL functions.
 module Kellisp where
 
--- |For now, a LispVal is simply a String, but this will be expanded
+import System.Console.Haskeline
+
+-- | For now, a LispVal is simply a String, but this will be expanded
 type LispVal = String
 
--- |Reads Text into a LispVal
--- TODO: Update with Parser, may have to change signature to Text
+-- | Reads Text into a LispVal
+--  TODO: Update with Parser, may have to change signature to Text
 readVal :: String -> LispVal
 readVal = id
 
--- |Evaluates a LispVal
+-- | Evaluates a LispVal
 eval :: LispVal -> LispVal
 eval = id
 
--- |Pretty-prints some LispVal
+-- | Pretty-prints some LispVal
 printVal :: LispVal -> String
 printVal = id
 
--- |Reads Text as a LispVal, which is evaluated and printed
+-- | Reads Text as a LispVal, which is evaluated and printed
 rep :: String -> String
 rep = printVal . eval . readVal
 
--- |Read eval print loop
+-- | Read eval print loop
 repl :: IO ()
-repl = do s <- getLine
-          putStrLn $ rep s
-          repl
+repl = runInputT defaultSettings loop
+  where
+    loop = do
+      s <- getInputLine "λ> "
+      case s of
+        -- if you wanted to add special commands, here is where that could be done
+        -- just add another pattern for the Just case, like Just "quit", etc
+        Nothing -> return ()
+        Just input -> do
+          outputStrLn $ rep input
+          loop
