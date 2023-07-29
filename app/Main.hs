58d0f8b5c6aa6f01882ce9d608fa7d9611cc0a4a
Zack Eisbach <zack.eisbach@gmail.com>
2023-07-29 12:53:10 -0400

    add do-nothing REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Kellisp
+import Kellisp ( repl )
 
+-- |Interpreter entry point: enters the REPL
+-- TODO: parse command line arguments, either REPL or interpret a file
 main :: IO ()
-main = func
+main = repl
--- src/Kellisp.hs
+++ src/Kellisp.hs
@@ -1,5 +1,31 @@
+{-|
+Contains all outward-facing functions used to interact with Kellisp.
+For now, this is limited to REPL functions.
+-}
 module Kellisp where
 
--- | some helper function to be used by main
-func :: IO ()
-func = putStrLn "Hello World!"
+-- |For now, a LispVal is simply a String, but this will be expanded
+type LispVal = String
+
+-- |Reads Text into a LispVal
+-- TODO: Update with Parser, may have to change signature to Text
+readVal :: String -> LispVal
+readVal = id
+
+-- |Evaluates a LispVal
+eval :: LispVal -> LispVal
+eval = id
+
+-- |Pretty-prints some LispVal
+printVal :: LispVal -> String
+printVal = id
+
+-- |Reads Text as a LispVal, which is evaluated and printed
+rep :: String -> String
+rep = printVal . eval . readVal
+
+-- |Read eval print loop
+repl :: IO ()
+repl = do s <- getLine
+          putStrLn $ rep s
+          repl
