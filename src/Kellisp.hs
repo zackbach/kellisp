c7a99dbae97abb3eeacce1b09e2ae232525bb0db
Zack Eisbach <zack.eisbach@gmail.com>
2023-08-03 13:21:00 -0400

    refactor REPL, Fixes #1

--- src/Kellisp.hs
+++ src/Kellisp.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 -- |
 -- Contains all outward-facing functions used to interact with Kellisp.
 -- For now, this is limited to REPL functions.
@@ -19,6 +17,43 @@ import           System.Console.Haskeline
 
 import           Text.Megaparsec (parse)
 
+-- | Read eval print loop
+repl :: IO ()
+repl = runInputT defaultSettings $ loop defaultEnv
+
+-- | one iteration of the REPL, evaluated in the given environment
+-- mutually recursive with handleInput
+loop :: Env -> InputT IO ()
+loop env = do
+  s <- getInputLine "λ> "
+  case s of
+    -- if you wanted to add special commands, here is where that could be done
+    -- just add another pattern for the Just case, like Just "quit", etc
+    Nothing    -> return ()
+    Just input -> handleInput env $ T.pack input
+
+-- TODO: these helpers could probably be cleaned up a bit
+-- and their signatures could be enhanced / generalized
+
+-- | attempts to evaluate the input in the given environment, catching errors
+-- and passing in the updated environment to the next loop iteration
+handleInput :: Env -> T.Text -> InputT IO ()
+handleInput env input = do
+  res <- liftIO $ try $ evalInput env input
+  case res of
+    Left (err :: SomeException) -> outputStrLn (show err) >> loop env
+    Right (env', val)           -> outputStrLn (show val) >> loop env'
+
+-- | parses and evaluates text in the given input, returning
+-- both the resulting LispVal and the new environment
+evalInput :: Env -> T.Text -> IO (Env, LispVal)
+evalInput env input = runReaderT comp env
+  where
+    comp = do
+      result <- unEval $ readEval input
+      evalEnv <- ask
+      return (evalEnv, result)
+
 -- | Reads and parses text into a LispVal that is evaluated
 -- note that the Eval monad is not actually run here, so we
 -- can pass in an initial context with `runReaderT` later
@@ -27,28 +62,3 @@ readEval txt = case parse parseLispVal "" txt of
   Left bundle -> throw $ ParseError bundle
   Right val   -> eval val
 
--- | Read eval print loop
-repl :: IO ()
-repl = runInputT defaultSettings $ loop defaultEnv
-  where
-    loop :: Env -> InputT IO ()
-    loop env = do
-      s <- getInputLine "λ> "
-      case s of
-        -- if you wanted to add special commands, here is where that could be done
-        -- just add another pattern for the Just case, like Just "quit", etc
-        Nothing    -> return ()
-        Just input -> do
-          -- here, we parse the input with readEval and unwrap into a ReaderT monad to run
-          -- but before we run, we also extract the environment to pass to the next loop
-          -- and we wrap in a try to handle any errors that may arise
-          res <- liftIO
-            $ try
-            -- yes, this is super gross... maybe add InputT to Eval to avoid
-            -- "leaving" and "re-entering" the monad stack in this function
-            $ runReaderT
-              (liftM2 (,) ask $ unEval $ readEval $ T.pack input)
-              env
-          case res of
-            Left (err :: SomeException) -> outputStrLn (show err) >> loop env
-            Right (env', val)           -> outputStrLn (show val) >> loop env'
